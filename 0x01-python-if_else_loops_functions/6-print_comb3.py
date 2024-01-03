e063977afea9589d922aee299d5dc85f7ec99e38
siddibank <bankolesodiq222@gmail.com>
2024-01-03 11:23:17 -0800

    messages

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+for digit1 in range(10):
+    for digit2 in range(digit1 + 1, 10):
+        if digit1 < digit2:
+            if digit1 == 8:
+                print("{:d}{:d}".format(digit1, digit2))
+            else:
+                print("{:d}{:d}".format(digit1, digit2), end=", ")

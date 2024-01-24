1c14658289e79ccea811a3a96415032d4fe8c9e7
siddibank <bankolesodiq222@gmail.com>
2024-01-24 15:53:18 -0800

    meesages

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    count, index = 0, 0
+    while index < x:
+        try:
+            print("{:d}".format(my_list[index]), end='')
+            count += 1
+        except (ValueError, TypeError):
+            pass
+        index += 1
+    print()
+    return count

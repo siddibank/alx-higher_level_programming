7a44193aa6e06a6e982de35c0dcad92d136f6c41
siddibank <bankolesodiq222@gmail.com>
2024-02-09 15:46:38 -0800

    messages

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    """
+    replaces all occurrences of an element by another in a new list.
+    Args:
+        my_list: initial list
+        search: element to replace in the list
+        replace: new element
+    Returns:
+        new list
+    """
+    new_list = []
+
+    for i in my_list:
+        if i == search:
+            new_list.append(replace)
+        else:
+            new_list.append(i)
+
+    return new_list

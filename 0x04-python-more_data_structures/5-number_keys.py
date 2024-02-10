b82f56761e6af262272b77228ae6d870a9ca0cf3
siddibank <bankolesodiq222@gmail.com>
2024-02-10 15:52:33 -0800

    messages

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    """
+    returns the number of keys in a dictionary.
+    Args:
+        a_dictionary
+    Returns:
+        number of keys in a dictionary.
+    """
+
+    return len(a_dictionary)
